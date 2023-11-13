5b988150dd728ee277c923be4c6ee55c569e79a4
JSDevadathan <jsdevadathan@gmail.com>
2023-11-13 10:37:03 +0530

    CrossOrigin Added

--- src/main/java/com/edstem/projecttracker/controller/TicketController.java
+++ src/main/java/com/edstem/projecttracker/controller/TicketController.java
@@ -5,6 +5,7 @@ import com.edstem.projecttracker.contract.response.TicketResponse;
 import com.edstem.projecttracker.service.TicketService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor()
 @RequestMapping("/tickets")
+@CrossOrigin("*")
 public class TicketController {
     private final TicketService ticketService;
 
